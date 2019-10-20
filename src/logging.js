c0ae2de81eec85d8d127331451a634fa0f8a16f5
Tommy Dorsey <tdorsey@users.noreply.github.com>
2019-10-20 07:33:55 -0400

    feat: Add logging with debug and pino
    Use pino, debug, and pino-debug extensions to demo loglevels

---
+++ src/logging.js
@@ -0,0 +1,74 @@
+import { env } from "./environment"
+
+const pino = require('pino')
+const debug = require('debug');
+const pinoDebug = require('pino-debug')
+const logName = 'react-demo';
+
+const overrideLogging = () => {
+
+  for ( let level in levels) { 
+     console[level] = debug;
+   }
+}
+
+const buildPinoConfig = () => {
+  let config = { name: "demo", level: env.LOG_LEVEL || 'debug'}
+  return config
+}
+
+
+const loggerFactory = () => {
+
+	let config = buildPinoConfig()
+
+	let logger = pino(config)
+ 	return logger
+}
+
+
+const logger = loggerFactory();
+
+pinoDebug(logger, {
+  auto: true, // default
+  map: {
+    'react-demo:*': 'trace',
+    'react-demo-lib:*': 'warn',
+  },
+  skip: [
+    'some-other-thing' ]
+});
+
+var debugLogger = require('debug')(logName);
+
+
+
+const enableDebug = () => {
+
+debug.disable();
+debug.enable(`${logName}:*`);
+
+
+pinoDebug(logger, {
+  auto: true, // default
+  map: {
+    '${logName}:*': 'trace',
+    'react-demo-lib:*': 'warn',
+    '*': 'silent' // everything else - silent
+  }
+})
+
+
+}
+
+if (env.name !== 'production') {
+
+	const logger = loggerFactory();
+	enableDebug();
+	overrideLogging(true);
+}
+
+export const { levels } = ["trace", "debug", "info", "log", "warn", "error"];
+
+
+export default levels 
