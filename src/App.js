779a35ec07900c34e6b6149a288c21eb0c5c0cc8
Tommy Dorsey <tdorsey@users.noreply.github.com>
2019-10-21 00:19:24 -0400

    feat: Display environment configuration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withConfig } from "./WithConfig"
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { withConfig } from "./WithConfig";
+import { EnvironmentDisplay } from "./EnvironmentDisplay";
 function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is running in {props.config.name}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <EnvironmentDisplay environment={props.config} />
+        </header>
+      </div>
+    </>
   );
 }
 
